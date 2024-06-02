279b46a04d2319fe458cb828ee333d9385beb00b
xyzgithome <2584118953@qq.com>
2024-06-02 20:39:03 +0800

    网关基础上下文的编写

---
+++ gateway-core/src/main/java/com/wym/core/context/BaseContext.java
@@ -0,0 +1,142 @@
+package com.wym.core.context;
+
+import io.netty.channel.ChannelHandlerContext;
+import org.springframework.util.CollectionUtils;
+
+import javax.swing.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
+
+public class BaseContext implements IContext {
+    protected final String protocol;
+
+    // 状态多线程情况下考虑使用volatile，多线程下可见性和防止指令重排
+    protected volatile int status = IContext.RUNNING;
+
+    // Netty上下文
+    protected final ChannelHandlerContext nettyCtx;
+
+    // netty上下文参数
+    protected final Map<String, Object> attributes = new HashMap<>();
+
+    // 请求过程中发生的异常
+    protected Throwable throwable;
+
+    // 是否保持长连接
+    protected final boolean keepAlive;
+
+    // 存放回调函数集合
+    protected List<Consumer<IContext>> completedCallbackList;
+
+    // 定义是否已经释放资源
+    protected final AtomicBoolean requestReleased = new AtomicBoolean(false);
+
+    public BaseContext(String protocol, ChannelHandlerContext nettyCtx, boolean keepAlive) {
+        this.protocol = protocol;
+        this.nettyCtx = nettyCtx;
+        this.keepAlive = keepAlive;
+    }
+
+    @Override
+    public void running() {
+        status = IContext.RUNNING;
+    }
+
+    @Override
+    public void written() {
+        status = IContext.WRITTEN;
+    }
+
+    @Override
+    public void completed() {
+        status = IContext.COMPLETED;
+    }
+
+    @Override
+    public void terminated() {
+        status = IContext.TERMINATED;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return status == IContext.RUNNING;
+    }
+
+    @Override
+    public boolean isWritten() {
+        return status == IContext.WRITTEN;
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return status == IContext.COMPLETED;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return status == IContext.TERMINATED;
+    }
+
+    @Override
+    public String getProtocol() {
+        return this.protocol;
+    }
+
+    @Override
+    public Object getRequest() {
+        return null;
+    }
+
+    @Override
+    public Object getResponse() {
+        return null;
+    }
+
+    @Override
+    public void setResponse(Object response) {
+
+    }
+
+    @Override
+    public Throwable getThrowable() {
+        return this.throwable;
+    }
+
+    @Override
+    public void setThrowable(Throwable throwable) {
+        this.throwable = throwable;
+    }
+
+    @Override
+    public ChannelHandlerContext getNettyCtx() {
+        return this.nettyCtx;
+    }
+
+    @Override
+    public boolean isKeepAlive() {
+        return this.keepAlive;
+    }
+
+    @Override
+    public boolean releaseRequest() {
+        return false;
+    }
+
+    @Override
+    public void setCompleteCallback(Consumer<IContext> consumer) {
+        if (CollectionUtils.isEmpty(completedCallbackList)) {
+            completedCallbackList = new ArrayList<>();
+        }
+        completedCallbackList.add(consumer);
+    }
+
+    @Override
+    public void invokeCompleteCallback(Consumer<IContext> consumer) {
+        if (CollectionUtils.isEmpty(completedCallbackList)) {
+            return;
+        }
+
+        completedCallbackList.forEach(completedCallback -> completedCallback.accept(this));
+    }
+}
--- gateway-core/src/main/java/com/wym/core/context/IContext.java
+++ gateway-core/src/main/java/com/wym/core/context/IContext.java
@@ -1,9 +1,118 @@
 package com.wym.core.context;
 
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.function.Consumer;
+
 /**
  * 核心上下文接口定义
  */
 public interface IContext {
+    /**
+     * 运行中
+     */
+    int RUNNING = 0;
 
-    
+    /**
+     * 运行过程中发生错误，
+     * 对其进行标记，告诉我们请求已经结束，需要返回客户端
+     */
+    int WRITTEN = 1;
+
+    /**
+     * 标记写回成功，防止并发情况下的多次写回
+     */
+    int COMPLETED = 2;
+
+    /**
+     * 标识网关请求结束
+     */
+    int TERMINATED = -1;
+
+    /**
+     * 设置上下文状态为运行中
+     */
+    void running();
+
+    /**
+     * 设置上下文状态为标记写回
+     */
+    void written();
+
+    /**
+     * 设置上下文状态为标记写回成功
+     */
+    void completed();
+
+    /**
+     * 设置上下文状态为请求结束
+     */
+    void terminated();
+
+    /**
+     * 判断网关状态是否运行中
+     * @return true false
+     */
+    boolean isRunning();
+
+    /**
+     * 判断网关状态是否标记写回
+     * @return true false
+     */
+    boolean isWritten();
+
+    /**
+     * 判断网关状态是否标记写回成功
+     * @return true false
+     */
+    boolean isCompleted();
+
+    /**
+     * 判断网关状态是否请求结束
+     * @return true false
+     */
+    boolean isTerminated();
+
+    String getProtocol();
+    Object getRequest();
+    Object getResponse();
+    void setResponse(Object response);
+    Throwable getThrowable();
+    void setThrowable(Throwable throwable);
+
+    /**
+     * 获取netty上线文
+     *
+     * @return ctx
+     */
+    ChannelHandlerContext getNettyCtx();
+
+    /**
+     * 是否长连接
+     *
+     * @return true false
+     */
+    boolean isKeepAlive();
+
+    /**
+     * 释放请求资源
+     *
+     * @return
+     */
+    boolean releaseRequest();
+
+    /**
+     * 设置写回接收回调函数
+     *
+     * @param consumer
+     */
+    void setCompleteCallback(Consumer<IContext> consumer);
+
+    /**
+     * 执行写回接收回调函数
+     *
+     * @param consumer
+     */
+    void invokeCompleteCallback(Consumer<IContext> consumer);
+
 }
