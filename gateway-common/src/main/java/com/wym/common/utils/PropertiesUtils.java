4432ad28e4f0e63a1d1b63a13f6d6c81b08a7a9d
xyzgithome <2584118953@qq.com>
2024-06-04 22:02:10 +0800

    网关核心配置对象编写

--- gateway-common/src/main/java/com/wym/common/utils/PropertiesUtils.java
+++ gateway-common/src/main/java/com/wym/common/utils/PropertiesUtils.java
@@ -1,53 +1,69 @@
 package com.wym.common.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.lang.reflect.Method;
+import java.util.Objects;
 import java.util.Properties;
 
+@Slf4j
 public class PropertiesUtils {
-
-    public static void properties2Object(final Properties p, final Object object, String prefix) {
+    public static void properties2Object(final Properties ps, final Object object, String prefix) {
         Method[] methods = object.getClass().getMethods();
         for (Method method : methods) {
-            String mn = method.getName();
-            if (mn.startsWith("set")) {
-                try {
-                    String tmp = mn.substring(4);
-
-                    String first = mn.substring(3, 4);
-
-                    String key = prefix + first.toLowerCase() + tmp;
-                    String property = p.getProperty(key);
-                    if (property != null) {
-                        Class<?>[] pt = method.getParameterTypes();
-                        if (pt != null && pt.length > 0) {
-                            String cn = pt[0].getSimpleName();
-                            Object arg = null;
-                            if (cn.equals("int") || cn.equals("Integer")) {
-                                arg = Integer.parseInt(property);
-                            } else if (cn.equals("long") || cn.equals("Long")) {
-                                arg = Long.parseLong(property);
-                            } else if (cn.equals("double") || cn.equals("Double")) {
-                                arg = Double.parseDouble(property);
-                            } else if (cn.equals("boolean") || cn.equals("Boolean")) {
-                                arg = Boolean.parseBoolean(property);
-                            } else if (cn.equals("float") || cn.equals("Float")) {
-                                arg = Float.parseFloat(property);
-                            } else if (cn.equals("String")) {
-                                arg = property;
-                            } else {
-                                continue;
-                            }
-                            method.invoke(object, arg);
-                        }
-                    }
-                } catch (Throwable ignored) {
+            try {
+                String mn = method.getName();
+                if (!mn.startsWith("set")) {
+                    continue;
                 }
+
+                String tmp = mn.substring(4);
+                String first = mn.substring(3, 4);
+                String key = prefix + first.toLowerCase() + tmp;
+                String property = ps.getProperty(key);
+                if (Objects.isNull(property)) {
+                    continue;
+                }
+
+                Class<?>[] pt = method.getParameterTypes();
+                if (ArrayUtils.isEmpty(pt)) {
+                    continue;
+                }
+
+                String cn = pt[0].getSimpleName();
+                Object arg;
+                switch (cn) {
+                    case "int":
+                    case "Integer":
+                        arg = Integer.parseInt(property);
+                        break;
+                    case "long":
+                    case "Long":
+                        arg = Long.parseLong(property);
+                        break;
+                    case "double":
+                    case "Double":
+                        arg = Double.parseDouble(property);
+                        break;
+                    case "boolean":
+                    case "Boolean":
+                        arg = Boolean.parseBoolean(property);
+                        break;
+                    case "float":
+                    case "Float":
+                        arg = Float.parseFloat(property);
+                        break;
+                    case "String":
+                        arg = property;
+                        break;
+                    default:
+                        continue;
+                }
+                method.invoke(object, arg);
+            } catch (Throwable t) {
+                log.error("parse property error, t:", t);
             }
         }
     }
-    
-    public static void properties2Object(final Properties p, final Object object) {
-        properties2Object(p, object, "");
-    }
- 
 }
