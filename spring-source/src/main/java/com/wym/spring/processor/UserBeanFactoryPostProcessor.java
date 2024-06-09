df0d008584e2a99e8efbe876f0d73552713a7e5e
xyzgithome <2584118953@qq.com>
2024-06-09 22:13:05 +0800

    BeanFactoryPostProcessor接口使用

---
+++ spring-source/src/main/java/com/wym/spring/processor/UserBeanFactoryPostProcessor.java
@@ -0,0 +1,17 @@
+package com.wym.spring.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BeanDefinition userServiceBD = beanFactory.getBeanDefinition("userService");
+
+        userServiceBD.setScope(BeanDefinition.SCOPE_PROTOTYPE);
+    }
+}
